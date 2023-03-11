a23ded0d2b380ce2379e0acb2768e1c7ce6fedfb
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-11 16:10:12 +0100

    Format code

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-ReactDOM.render(<ul><li>Apple</li><li>Oranges</li></ul>, document.getElementById("root"))
+ReactDOM.render(
+    <ul><li>Apple</li><li>Oranges</li></ul>, 
+    document.getElementById("root")
+)
