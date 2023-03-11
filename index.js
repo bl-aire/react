9754f0d82bd56b3b5906b9ece2178d69e8744351
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-11 16:55:21 +0100

    React composability

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function MyContent() {
     return (
-        <p>This is my custom content.</p>
+        <p>This is my content component.</p>
+    )
+}
+
+function MyFooter() {
+    return (
+        <p>This is my footer component.</p>
     )
 }
 
 ReactDOM.render(
     <div>
         <MyContent />
+        < MyFooter />
     </div>, 
     document.getElementById("root")
 )
