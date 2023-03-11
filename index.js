94916b58f40ab220215a8ee3347ed0a933c8492b
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-11 18:06:34 +0100

    Start info site project

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const header = (
-    <nav>
-        <h1>Blaire</h1>
-        <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-)
+function Nav() {
+    return (
+        <nav>
+            <img src="./React-icon.svg.png" width="100px" />
+        </nav>
+    )
+}
+
+function Content() {
+    return (
+        <div>
+            <h1>Fun facts about Blessing</h1>
+            <ul>
+                <li>I love coding</li>
+                <li>I love Ice cream</li>
+                <li>I love learning</li>
+            </ul>
+        </div>
+    )
+}
 
 ReactDOM.render(
-    header, 
+    <div>
+        <Nav />
+        <Content />
+    </div>, 
     document.getElementById("root")
 )
